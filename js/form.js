e748bfd89b72ed8fec8a2a36d18e5fbe5a0d6838
evellynfss <evellyn.elly08@gmail.com>
2025-06-04 14:09:35 -0300

    modificação

--- js/form.js
+++ js/form.js
@@ -1,27 +1,28 @@
 
-//class contato
-
 class contato {
-    
+    constructor(nome, sobrenome, email, cpf, telefone, preferenciaContato) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.preferenciaContato = preferenciaContato;
+    }
 }
 
 function Post(form) {
+    let data = new contato(
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value, 
+        form.elements.namedItem("email").value, 
+        form.elements.namedItem("cpf").value, 
+        form.elements.namedItem("telefone").value, 
+        form.elements.namedItem("contato").value
+    );
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
+    console.log(data); 
+    alert('Obrigado sr(a) ' + data.nome + '' + data.sobrenome + ', seus dados foram encaminhados com sucesso!');
 
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
+    form.reset(); 
+    return false; 
 }
